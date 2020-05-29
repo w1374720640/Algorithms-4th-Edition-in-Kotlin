1d13218d892d4f349d7434b261877c5280085035
wangjingzhao <1374720640@qq.com>
2020-05-29 16:01:44 +0800

    添加1.1.35

--- src/capter1/exercise1_1.kt
+++ src/capter1/exercise1_1.kt
@@ -1,5 +1,7 @@
 import java.lang.Exception
 import java.math.BigDecimal
+import kotlin.math.abs
+import kotlin.random.Random
 
 //判断数组中的所有值是否相同
 fun ex3(args: Array<String>) {
@@ -505,3 +507,46 @@ fun ex33_e(y: Array<Int>, a: Array<Array<Int>>) {
     }
     ex33_b(matrix, a)
 }
+
+//计算掷骰子时，两个骰子之和理论上的概率分布
+//并模拟投掷N次骰子，比较实际概率和理论概率的区别，当N多大时，理论值和实际值误差小于0.001
+//经测试，当N为一百万时可以保证多次运行的理论值和实际值最大误差小于0.001
+fun ex35(N: Int) {
+    val sides = 6
+    //理论上的概率分布
+    val theoreticalValue = Array(sides * 2 + 1) { 0.0 }
+    for (i in 1..sides) {
+        for (j in 1..sides) {
+            theoreticalValue[i + j] += 1.0
+        }
+    }
+    for (i in 2 until theoreticalValue.size) {
+        theoreticalValue[i] /= 36.0
+    }
+    println("theoreticalValue=${theoreticalValue.joinToString { BigDecimal(it).setScale(4, BigDecimal.ROUND_HALF_UP).toString() }}")
+    println()
+
+    //实际概率
+    val actualValue = Array(sides * 2 + 1) { 0.0 }
+    repeat(N) {
+        val a = Random.Default.nextInt(sides) + 1
+        val b = Random.Default.nextInt(sides) + 1
+        actualValue[a + b] += 1.0
+    }
+    for (i in 2 until actualValue.size) {
+        actualValue[i] /= N.toDouble()
+    }
+    println("actualValue     =${actualValue.joinToString { BigDecimal(it).setScale(4, BigDecimal.ROUND_HALF_UP).toString() }}")
+    println()
+
+    //最大误差
+    var maximumError = 0.0
+    for (i in 2 until theoreticalValue.size) {
+        val diff = abs(theoreticalValue[i] - actualValue[i])
+        if (diff > maximumError) maximumError = diff
+    }
+    println("maximumError=${BigDecimal(maximumError).setScale(5, BigDecimal.ROUND_HALF_UP)}")
+}
+
+
+
