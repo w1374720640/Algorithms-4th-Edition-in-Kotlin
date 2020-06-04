e7243dc0186a3e9fb3bcfcc6931a9ed42ac748fc
wangjingzhao <1374720640@qq.com>
2020-06-04 10:52:27 +0800

    添加1.2.6、1.2.9、1.2.10

---
+++ src/capter1/exercise1_2/ex6.kt
@@ -0,0 +1,13 @@
+package capter1.exercise1_2
+
+//检查两个字符串是否互为回环变位
+//回环变位是指：a中的字符 循环移动 任意位置之后能够得到另一个字符串b，则a被称为b的回环变位
+fun ex6(a: String, b: String): Boolean {
+    return (a + a).indexOf(b) != -1 && (b + b).indexOf(a) != -1
+}
+
+fun main() {
+    val a = "ACTGACG"
+    val b = "TGACGAC"
+    println(ex6(a, b))
+}
