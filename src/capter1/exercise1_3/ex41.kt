41d87e871048a4b815b8edaea5e36cd46fd18267
wangjingzhao <1374720640@qq.com>
2020-06-13 13:45:30 +0800

    添加1.3.41、1.3.43

---
+++ src/capter1/exercise1_3/ex41.kt
@@ -0,0 +1,34 @@
+package capter1.exercise1_3
+
+import edu.princeton.cs.algs4.Queue
+import extensions.inputPrompt
+import extensions.readAllStrings
+
+//复制队列
+fun <T> Queue<T>.copy(queue: Queue<T>): Queue<T> {
+    val copyQueue = Queue<T>()
+    val size = queue.size()
+    //这里将原队列先出列，再分别入列到两个队列中，
+    //也可以用迭代器访问，只需要入列一次
+    repeat(size) {
+        val value = queue.dequeue()
+        queue.enqueue(value)
+        copyQueue.enqueue(value)
+    }
+    return copyQueue
+}
+
+fun main() {
+    inputPrompt()
+    val array = readAllStrings()
+    val originQueue = Queue<String>()
+    array.forEach {
+        originQueue.enqueue(it)
+    }
+    val copyQueue = originQueue.copy(originQueue)
+    if (!originQueue.isEmpty) {
+        originQueue.dequeue()
+    }
+    println("origin queue = ${originQueue.joinToString()}")
+    println("copy queue = ${copyQueue.joinToString()}")
+}
