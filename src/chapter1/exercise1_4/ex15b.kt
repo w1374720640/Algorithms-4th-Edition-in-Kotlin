44b36c786ce951cb1bc05c3f5a6ff715f9e238c7
wangjingzhao <1374720640@qq.com>
2020-07-11 18:29:52 +0800

    重写练习1.4.15

--- src/chapter1/exercise1_4/ex15b.kt
+++ src/chapter1/exercise1_4/ex15b.kt
@@ -1,32 +1,49 @@
 package chapter1.exercise1_4
 
+import extensions.inputPrompt
+import extensions.readAllInts
+
 /**
  * 求三个数相加和为0的组合数量，要求使用平方级算法，参考ex15a
  */
 fun ex15b(array: IntArray): Long {
-    array.sort()
+    fun getZeroCount(array: IntArray): Int {
+        var count = 0
+        array.forEach { if (it == 0) count++ }
+        return count
+    }
+
     var count = 0L
+    val zeroCount = getZeroCount(array)
+    if (zeroCount > 2) {
+        count += combination(zeroCount, 3)
+    }
     for (i in array.indices) {
-        if (array[i] > 0) break
-        //若两个数相加等于A，那这两个值一定在二分之A的两侧
-        val pair = findLeftAndRight(-array[i] / 2, array)
-        var left = pair.first
-        var right = pair.second
-        while (left > i && right < array.size) {
+        if (array[i] >= 0) break
+        var left = i + 1
+        var right = array.size - 1
+        while (left < right) {
             when {
-                array[left] + array[right] + array[i] == 0 -> {
-                    count++
-                    if (left - 1 > i && array[left] == array[left - 1]) {
-                        left--
-                    } else {
-                        right++
+                array[i] + array[left] + array[right] > 0 -> right--
+                array[i] + array[left] + array[right] < 0 -> left++
+                else -> {
+                    var leftEqualCount = 1
+                    val leftValue = array[left]
+                    //两数和为0不用判断left<right-1，但是三数和需要判断，因为array[left]可能等于array[right]
+                    while (left < right - 1 && array[left + 1] == leftValue) {
+                        leftEqualCount++
+                        left++
                     }
-                }
-                array[left] + array[right] + array[i] > 0 -> {
-                    left--
-                }
-                array[left] + array[right] + array[i] < 0 -> {
-                    right++
+
+                    var rightEqualCount = 1
+                    val rightValue = array[right]
+                    while (right > left + 1 && array[right - 1] == rightValue) {
+                        rightEqualCount++
+                        right--
+                    }
+                    count += leftEqualCount * rightEqualCount
+                    left++
+                    right--
                 }
             }
         }
@@ -35,5 +52,10 @@ fun ex15b(array: IntArray): Long {
 }
 
 fun main() {
-    timeRatio { ex15b(it) }
+    inputPrompt()
+    // -4 -3 -2 -2 -1 -1 -1 -1 0 0 0 0 0 1 1 2 4 4 4 5 5
+    val array = readAllInts()
+    println("ThreeSum count=${threeSum(array)}")
+    array.sort()
+    println("ex15b count=${ex15b(array)}")
 }
